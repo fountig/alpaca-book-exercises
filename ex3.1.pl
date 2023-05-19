d8f98efb72510baba9d9edb5d90a78a23cf52cfe
George Fountis <118578531+shadowrun96@users.noreply.github.com>
2023-05-19 20:36:27 +0300

    Add files via upload
    Investigate why we needed two parens in grep statement. Probably because of list context.

---
+++ ex3.1.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#
+# Exercise 3.1
+#
+# Write a program that takes a list of filenames on the command line
+# and uses grep to select the ones whose size is less than 1,000 bytes. Use map to
+# transform the strings in this list, putting four space characters in front of each and
+# a newline character after. Print the resulting list.
+
+@filenames = <@ARGV>;
+
+@less_than_1K = grep { ((stat $_)[7]) < 1000 } @filenames; # Investigate further why we need 2 parens here. 
+
+print @less_than_1K;
+
